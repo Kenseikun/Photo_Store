cf7d0f44610a351f388c238013b3b0842f206506
kenseikun <sebastian.dziechciarz@wp.pl>
2021-01-01 19:28:20 +0100

    products data add

---
+++ src/localData.js
@@ -0,0 +1,203 @@
+import canon60d from "./assets/images/canon60d.jpg";
+import canonFT from "./assets/images/canonft.jpg";
+import nikonF3 from "./assets/images/nikonf3.jpg";
+import nikonF4 from "./assets/images/nikonf4.jpg";
+import sonyA7 from "./assets/images/sonya7.jpg";
+import canon18135 from "./assets/images/canon18-135.jpg";
+import canon24 from "./assets/images/canon24.jpg";
+import canon28 from "./assets/images/canon28.jpg";
+import nikkor24 from "./assets/images/nikkor24.jpg";
+import nikkor2450 from "./assets/images/nikkor24-50.jpg";
+import nikkor2470 from "./assets/images/nikkor24-70.jpg";
+import sigma2470 from "./assets/images/sigma24-70.jpg";
+import sigma35 from "./assets/images/sigma35.jpg";
+import canon470ex from "./assets/images/canon470ex.jpg";
+import nissin from "./assets/images/nissin.jpg";
+import sb900 from "./assets/images/sb900.jpg";
+
+export const localData = [
+  // CAMERAS
+
+  {
+    productId: 1,
+    productName: "Canon 60d",
+    productPrice: 1550,
+    productQuantity: 1,
+    productCategory: "cameras",
+    freeDelivery: true,
+    productDescripion:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Eos fuga labore repudiandae voluptatem maiores cupiditate provident ullam expedita culpa iste adipisci assumenda saepe exercitationem, perspiciatis tenetur, sunt necessitatibus, voluptates alias?",
+    productImage: canon60d,
+  },
+  {
+    productId: 2,
+    productName: "Canon FT",
+    productPrice: 435,
+    productQuantity: 1,
+    productCategory: "cameras",
+    freeDelivery: true,
+    productDescripion:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Eos fuga labore repudiandae voluptatem maiores cupiditate provident ullam expedita culpa iste adipisci assumenda saepe exercitationem, perspiciatis tenetur, sunt necessitatibus, voluptates alias?",
+    productImage: canonFT,
+  },
+  {
+    productId: 3,
+    productName: "Nikon F3",
+    productPrice: 1230,
+    productQuantity: 1,
+    productCategory: "cameras",
+    freeDelivery: true,
+    productDescripion:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Eos fuga labore repudiandae voluptatem maiores cupiditate provident ullam expedita culpa iste adipisci assumenda saepe exercitationem, perspiciatis tenetur, sunt necessitatibus, voluptates alias?",
+    productImage: nikonF3,
+  },
+  {
+    productId: 4,
+    productName: "Nikon F4",
+    productPrice: 1560,
+    productQuantity: 1,
+    productCategory: "cameras",
+    freeDelivery: true,
+    productDescripion:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Eos fuga labore repudiandae voluptatem maiores cupiditate provident ullam expedita culpa iste adipisci assumenda saepe exercitationem, perspiciatis tenetur, sunt necessitatibus, voluptates alias?",
+    productImage: nikonF4,
+  },
+  {
+    productId: 5,
+    productName: "Sony a7",
+    productPrice: 6280,
+    productQuantity: 1,
+    productCategory: "cameras",
+    freeDelivery: true,
+    productDescripion:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Eos fuga labore repudiandae voluptatem maiores cupiditate provident ullam expedita culpa iste adipisci assumenda saepe exercitationem, perspiciatis tenetur, sunt necessitatibus, voluptates alias?",
+    productImage: sonyA7,
+  },
+
+  //   LENSES
+
+  {
+    productId: 6,
+    productName: "Canon 18-135mm 2.8",
+    productPrice: 1685,
+    productQuantity: 1,
+    productCategory: "lenses",
+    freeDelivery: true,
+    productDescripion:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Eos fuga labore repudiandae voluptatem maiores cupiditate provident ullam expedita culpa iste adipisci assumenda saepe exercitationem, perspiciatis tenetur, sunt necessitatibus, voluptates alias?",
+    productImage: canon18135,
+  },
+  {
+    productId: 7,
+    productName: "Canon 24mm 1.8",
+    productPrice: 2280,
+    productQuantity: 1,
+    productCategory: "lenses",
+    freeDelivery: true,
+    productDescripion:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Eos fuga labore repudiandae voluptatem maiores cupiditate provident ullam expedita culpa iste adipisci assumenda saepe exercitationem, perspiciatis tenetur, sunt necessitatibus, voluptates alias?",
+    productImage: canon24,
+  },
+  {
+    productId: 8,
+    productName: "Canon 28mm 1.8",
+    productPrice: 4800,
+    productQuantity: 1,
+    productCategory: "lenses",
+    freeDelivery: true,
+    productDescripion:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Eos fuga labore repudiandae voluptatem maiores cupiditate provident ullam expedita culpa iste adipisci assumenda saepe exercitationem, perspiciatis tenetur, sunt necessitatibus, voluptates alias?",
+    productImage: canon28,
+  },
+  {
+    productId: 9,
+    productName: "Nikkor 24mm 1.8",
+    productPrice: 4350,
+    productQuantity: 1,
+    productCategory: "cameras",
+    freeDelivery: true,
+    productDescripion:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Eos fuga labore repudiandae voluptatem maiores cupiditate provident ullam expedita culpa iste adipisci assumenda saepe exercitationem, perspiciatis tenetur, sunt necessitatibus, voluptates alias?",
+    productImage: nikkor24,
+  },
+  {
+    productId: 10,
+    productName: "Nikkor 24-50mm 2.8",
+    productPrice: 3350,
+    productQuantity: 1,
+    productCategory: "cameras",
+    freeDelivery: true,
+    productDescripion:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Eos fuga labore repudiandae voluptatem maiores cupiditate provident ullam expedita culpa iste adipisci assumenda saepe exercitationem, perspiciatis tenetur, sunt necessitatibus, voluptates alias?",
+    productImage: nikkor2450,
+  },
+  {
+    productId: 11,
+    productName: "Nikkor 24-70mm 2.8",
+    productPrice: 5700,
+    productQuantity: 1,
+    productCategory: "cameras",
+    freeDelivery: true,
+    productDescripion:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Eos fuga labore repudiandae voluptatem maiores cupiditate provident ullam expedita culpa iste adipisci assumenda saepe exercitationem, perspiciatis tenetur, sunt necessitatibus, voluptates alias?",
+    productImage: nikkor2470,
+  },
+  {
+    productId: 12,
+    productName: "Sigma 24-70mm 2.8",
+    productPrice: 4780,
+    productQuantity: 1,
+    productCategory: "cameras",
+    freeDelivery: true,
+    productDescripion:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Eos fuga labore repudiandae voluptatem maiores cupiditate provident ullam expedita culpa iste adipisci assumenda saepe exercitationem, perspiciatis tenetur, sunt necessitatibus, voluptates alias?",
+    productImage: sigma2470,
+  },
+  {
+    productId: 13,
+    productName: "Sigma 35mm 1.4",
+    productPrice: 6730,
+    productQuantity: 1,
+    productCategory: "cameras",
+    freeDelivery: true,
+    productDescripion:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Eos fuga labore repudiandae voluptatem maiores cupiditate provident ullam expedita culpa iste adipisci assumenda saepe exercitationem, perspiciatis tenetur, sunt necessitatibus, voluptates alias?",
+    productImage: sigma35,
+  },
+
+  //   SPEEDLIGHTS
+
+  {
+    productId: 14,
+    productName: "Canon 470ex",
+    productPrice: 1200,
+    productQuantity: 1,
+    productCategory: "speedlights",
+    freeDelivery: true,
+    productDescripion:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Eos fuga labore repudiandae voluptatem maiores cupiditate provident ullam expedita culpa iste adipisci assumenda saepe exercitationem, perspiciatis tenetur, sunt necessitatibus, voluptates alias?",
+    productImage: canon470ex,
+  },
+  {
+    productId: 15,
+    productName: "Nissin",
+    productPrice: 635,
+    productQuantity: 1,
+    productCategory: "speedlights",
+    freeDelivery: true,
+    productDescripion:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Eos fuga labore repudiandae voluptatem maiores cupiditate provident ullam expedita culpa iste adipisci assumenda saepe exercitationem, perspiciatis tenetur, sunt necessitatibus, voluptates alias?",
+    productImage: nissin,
+  },
+  {
+    productId: 15,
+    productName: "Nikon sb900",
+    productPrice: 1665,
+    productQuantity: 1,
+    productCategory: "speedlights",
+    freeDelivery: true,
+    productDescripion:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Eos fuga labore repudiandae voluptatem maiores cupiditate provident ullam expedita culpa iste adipisci assumenda saepe exercitationem, perspiciatis tenetur, sunt necessitatibus, voluptates alias?",
+    productImage: sb900,
+  },
+];
