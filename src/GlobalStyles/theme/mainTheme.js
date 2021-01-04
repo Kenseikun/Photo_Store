689ded0fb594c0162cfee03e7744c5d89004bd3d
kenseikun <sebastian.dziechciarz@wp.pl>
2021-01-04 13:22:04 +0100

    gradient color add

--- src/GlobalStyles/theme/mainTheme.js
+++ src/GlobalStyles/theme/mainTheme.js
@@ -7,6 +7,10 @@ export const mainTheme = {
   gradientColors: {
     blue: "#052a66",
     violet: "#391f49",
+    dark: "#040d21",
+    background: "rgb(5,42,102)",
+    background:
+      "linear-gradient(140deg, rgba(5,42,102,1) 0%, rgba(4,13,33,1) 50%, rgba(57,31,73,1) 100%)",
   },
 
   colors: {
